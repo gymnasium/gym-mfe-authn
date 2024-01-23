7cd5d96cf3698d22f9134a5ef3d062d3a8b429ff
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-01-23 14:42:14 -0500

    Update GymApp.jsx

--- src/GymApp.jsx
+++ src/GymApp.jsx
@@ -4,10 +4,15 @@ import { getConfig } from '@edx/frontend-platform';
 import { AppProvider } from '@edx/frontend-platform/react';
 import { Helmet } from 'react-helmet';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import { GymHeader, messages as headerMessages } from '@edx/frontend-component-header';
-import Footer, { messages as footerMessages } from '@edx/frontend-component-footer';
-// import { GymHeader, messages as headerMessages } from '../frontend-component-header/src';
-// import Footer, { messages as footerMessages } from '../frontend-component-footer/src';
+
+import GymSettings from './gym-frontend-components/data/settings';
+import GymHeader from './gym-frontend-components/gym-header/GymHeader';
+
+const settings = await GymSettings();
+console.log(settings);
+const root = settings.urls.root; // should be same as marketing URL
+const config = getConfig();
+const css = `${root}/css/mfe.css`;
 
 import {
   Logistration, NotFoundPage, registerIcons, UnAuthOnlyRoute,
@@ -29,37 +34,36 @@ import { RecommendationsPage } from './recommendations';
 import { ResetPasswordPage } from './reset-password';
 import './index.scss';
 
-const config = getConfig();
-const settings = `${config.MARKETING_SITE_BASE_URL}/feeds/config.json`;
-const css = `${config.MARKETING_SITE_BASE_URL}/css/mfe.css`;
-
 registerIcons();
 
 const MainApp = () => (
   <AppProvider store={configureStore()}>
     <Helmet>
-      <link rel="shortcut icon" href={config.FAVICON_URL} type="image/x-icon" />
+      <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
       <link rel="stylesheet" href={css} />
     </Helmet>
-    <>
-      <GymHeader secondaryNav={false} />
-      <Switch>
-        <Route exact path="/">
-          <Redirect to={updatePathWithQueryParams(REGISTER_PAGE)} />
-        </Route>
-        <UnAuthOnlyRoute exact path={LOGIN_PAGE} render={() => <Logistration selectedPage={LOGIN_PAGE} />} />
-        <UnAuthOnlyRoute exact path={REGISTER_PAGE} component={Logistration} />
-        <UnAuthOnlyRoute exact path={RESET_PAGE} component={ForgotPasswordPage} />
-        <Route exact path={PASSWORD_RESET_CONFIRM} component={ResetPasswordPage} />
-        <Route exact path={AUTHN_PROGRESSIVE_PROFILING} component={ProgressiveProfiling} />
-        <Route exact path={RECOMMENDATIONS} component={RecommendationsPage} />
-        <Route path={PAGE_NOT_FOUND} component={NotFoundPage} />
-        <Route path="*">
-          <Redirect to={PAGE_NOT_FOUND} />
-        </Route>
-      </Switch>
-      <Footer />
-    </>
+
+    <GymHeader />
+
+    <main>
+      <div className="container"> 
+        <Switch>
+          <Route exact path="/">
+            <Redirect to={updatePathWithQueryParams(REGISTER_PAGE)} />
+          </Route>
+          <UnAuthOnlyRoute exact path={LOGIN_PAGE} render={() => <Logistration selectedPage={LOGIN_PAGE} />} />
+          <UnAuthOnlyRoute exact path={REGISTER_PAGE} component={Logistration} />
+          <UnAuthOnlyRoute exact path={RESET_PAGE} component={ForgotPasswordPage} />
+          <Route exact path={PASSWORD_RESET_CONFIRM} component={ResetPasswordPage} />
+          <Route exact path={AUTHN_PROGRESSIVE_PROFILING} component={ProgressiveProfiling} />
+          <Route exact path={RECOMMENDATIONS} component={RecommendationsPage} />
+          <Route path={PAGE_NOT_FOUND} component={NotFoundPage} />
+          <Route path="*">
+            <Redirect to={PAGE_NOT_FOUND} />
+          </Route>
+        </Switch>
+      </div>
+    </main>
   </AppProvider>
 );
 
