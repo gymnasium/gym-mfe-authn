2abaf65cb760807017d525e134fd29df5ad6b5d9
Amir Tadrisi <amirtds@gmail.com>
2024-10-28 09:39:00 -0500

    feat: add confirm email component and change order

---
+++ src/register/RegistrationFields/ConfirmEmailField/ConfirmEmailField.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useIntl } from '@edx/frontend-platform/i18n';
+import PropTypes from 'prop-types';
+
+import validateConfirmEmail from './validator';
+import { FormGroup } from '../../../common-components';
+import { clearRegistrationBackendError } from '../../data/actions';
+
+const ConfirmEmailField = (props) => {
+  const { formatMessage } = useIntl();
+  const dispatch = useDispatch();
+
+  const {
+    handleChange,
+    handleErrorChange,
+    emailValue,
+  } = props;
+
+  const handleOnBlur = (e) => {
+    const { value } = e.target;
+    const fieldError = validateConfirmEmail(value, emailValue, formatMessage);
+    if (fieldError) {
+      handleErrorChange('confirm_email', fieldError);
+    }
+  };
+
+  const handleOnFocus = () => {
+    handleErrorChange('confirm_email', '');
+    dispatch(clearRegistrationBackendError('confirm_email'));
+  };
+
+  return (
+    <FormGroup
+      {...props}
+      handleBlur={handleOnBlur}
+      handleFocus={handleOnFocus}
+    />
+  );
+};
+
+ConfirmEmailField.propTypes = {
+  emailValue: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleErrorChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  errorMessage: PropTypes.string,
+};
+
+ConfirmEmailField.defaultProps = {
+  errorMessage: '',
+};
+
+export default ConfirmEmailField;
---
+++ src/register/RegistrationFields/ConfirmEmailField/validator.js
@@ -0,0 +1,15 @@
+import messages from '../../messages';
+
+const validateConfirmEmail = (value, emailValue, formatMessage) => {
+  let fieldError = '';
+
+  if (!value) {
+    fieldError = formatMessage(messages['empty.confirm.email.field.error']);
+  } else if (value !== emailValue) {
+    fieldError = formatMessage(messages['email.do.not.match']);
+  }
+
+  return fieldError;
+};
+
+export default validateConfirmEmail;
