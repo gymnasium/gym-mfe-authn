3eb2e01c704293d843b34624a89c684950916629
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-11-25 13:50:56 -0500

    feat: add custom error pages, update gym-frontend
    See:
    - https://github.com/gymnasium/tracker/issues/581
    - https://github.com/gymnasium/tracker/issues/576

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {
-  APP_INIT_ERROR, APP_READY, initialize, mergeConfig, subscribe,
+  APP_INIT_ERROR, APP_READY, initialize, initError, mergeConfig, subscribe,
 } from '@edx/frontend-platform';
-import { ErrorPage } from '@edx/frontend-platform/react';
+
+import { ErrorPage } from '@openedx/gym-frontend';
 
 import configuration from './config';
 import messages from './i18n';
